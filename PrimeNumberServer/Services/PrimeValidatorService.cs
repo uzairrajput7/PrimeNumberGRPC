d97ce30e05a71e36258221ed94cd8585844b98b4
muhammad uzair <uzair@plutus21.com>
2023-06-15 00:55:04 -0400

    GRPC Server and Client initial commit with connection between both and prime validation

---
+++ PrimeNumberServer/Services/PrimeValidatorService.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+
+namespace PrimeNumberServer.Services
+{
+    public class PrimeValidatorService : PrimeValidator.PrimeValidatorBase
+    {
+        private readonly ILogger<PrimeValidatorService> _logger;
+        public static SortedList<long, bool> primes = new SortedList<long, bool>();
+
+        public PrimeValidatorService(ILogger<PrimeValidatorService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<IsPrimeNumberReply> IsPrimeNumber(PrimeNumberRequest request, ServerCallContext context)
+        {
+            var response = isPrimeNumber(request.Number);
+            return Task.FromResult(new IsPrimeNumberReply
+            {
+                Message = response ? "True" : "false"
+            });
+        }
+
+        private bool isPrimeNumber(long number)
+        {
+            if (primes.ContainsKey(number)) return true;
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var maxCheck = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= maxCheck; i++)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ RevolvingGamesClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using RevolvingGamesClient;
+
+
+// The port number must match the port of the gRPC server.
+using var channel = GrpcChannel.ForAddress("https://localhost:7250");
+var client = new PrimeValidator.PrimeValidatorClient(channel);
+var reply = await client.IsPrimeNumberAsync(new PrimeNumberRequest { Id = 1, Timestamp = 12, Number = 3 });
+Console.WriteLine("Is Prime Number: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
